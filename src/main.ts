069e637c70bb1238074ccbfc354067a97a819229
HopeNotDead <869132116@qq.com>
2022-10-17 00:44:30 +0800

    change_main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
+/*
+ * @Author: HopeNotDead 869132116@qq.com
+ * @Date: 2022-10-16 23:18:59
+ * @LastEditors: HopeNotDead 869132116@qq.com
+ * @LastEditTime: 2022-10-17 00:42:35
+ * @FilePath: \warmletheret\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './utils/rem'
+// import './utils/rem'
 // import eComponents from './components/swiper/index'
 
 createApp(App)
